8784dd0748e8dfc4477da5fab30423715282823f
KeshavReddyAvuduri <chennakesavareddy.avuduri@walkingtree.tech>
2021-05-29 22:11:05 +0530

    git ignore files inside platform

---
+++ platforms/android/app/build/generated/source/buildConfig/release/com/implen/nanophotometer/nanogap/tablet/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.implen.nanophotometer.nanogap.tablet;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.implen.nanophotometer.nanogap.tablet";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 26;
+  public static final String VERSION_NAME = "4.3.15670";
+}
